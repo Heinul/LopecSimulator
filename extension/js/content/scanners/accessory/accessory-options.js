6292db7b797d6952053a08b3e5eed46c53d80264
Heinul <henull.play@gmail.com>
2025-04-17 19:29:36 +0900

    코드정리

--- extension/js/content/scanners/accessory/accessory-options.js
+++ extension/js/content/scanners/accessory/accessory-options.js
@@ -16,21 +16,21 @@ LopecScanner.Scanners.Accessory.Options = (function() {
     DEALER: {
       // 목걸이 옵션 (option value 값 기준)
       NECKLACE: [
-        { value: "addDamagePer", description: "추가 피해" }, // 추가 피해
-        { value: "finalDamagePer", description: "적에게 주는 피해 증가" }, // 적에게 주는 피해 증가
-        { value: "value", description: "없음" } // 없음 옵션
+        { value: "addDamagePer", description: "추가 피해" },
+        { value: "finalDamagePer", description: "적에게 주는 피해 증가" },
+        { value: "value", description: "없음" } 
       ],
       // 귀걸이 옵션
       EARRING: [
-        { value: "atkPer", description: "공격력 %" }, // 공격력 %
-        { value: "weaponAtkPer", description: "무기 공격력 %" }, // 무기 공격력 %
-        { value: "value", description: "없음" } // 없음 옵션
+        { value: "atkPer", description: "공격력 %" },
+        { value: "weaponAtkPer", description: "무기 공격력 %" },
+        { value: "value", description: "없음" }
       ],
       // 반지 옵션
       RING: [
-        { value: "criticalDamagePer", description: "치명타 피해" }, // 치명타 피해
-        { value: "criticalChancePer", description: "치명타 적중률" }, // 치명타 적중률
-        { value: "value", description: "없음" } // 없음 옵션
+        { value: "criticalDamagePer", description: "치명타 피해" },
+        { value: "criticalChancePer", description: "치명타 적중률" },
+        { value: "value", description: "없음" }
       ]
     },
     
@@ -38,208 +38,23 @@ LopecScanner.Scanners.Accessory.Options = (function() {
     SUPPORTER: {
       // 목걸이 옵션
       NECKLACE: [
-        { value: "stigmaPer", description: "낙인력" }, // 낙인력
-        { value: "value", description: "세레나데, 신앙, 조화 게이지 획득량" }, // 세레나데/신성/조화 게이지 획득량 증가
-        { value: "value", description: "없음" } // 없음 옵션
+        { value: "stigmaPer", description: "낙인력" }, 
+        { value: "value", description: "세레나데, 신앙, 조화 게이지 획득량" },
+        { value: "value", description: "없음" } 
       ],
       // 귀걸이 옵션
       EARRING: [
-        { value: "weaponAtkPer", description: "무기 공격력 %" }, // 무기 공격력 %
-        { value: "weaponAtkPlus", description: "무기 공격력 +" }, // 무기 공격력 +
-        { value: "value", description: "없음" } // 없음 옵션
+        { value: "weaponAtkPer", description: "무기 공격력 %" }, 
+        { value: "weaponAtkPlus", description: "무기 공격력 +" }, 
+        { value: "value", description: "없음" }
       ],
       // 반지 옵션
       RING: [
-        { value: "atkBuff", description: "아군 공격력 강화 효과" }, // 아군 공격력 강화 효과
-        { value: "damageBuff", description: "아군 피해량 강화 효과" }, // 아군 피해량 강화 효과
-        { value: "value", description: "없음" } // 없음 옵션
+        { value: "atkBuff", description: "아군 공격력 강화 효과" }, 
+        { value: "damageBuff", description: "아군 피해량 강화 효과" }, 
+        { value: "value", description: "없음" }
       ]
-    },
-    
-    // 등급별 접두사 (호환성 유지)
-    GRADE_PREFIX: {
-      HIGH: "상",
-      MEDIUM: "중",
-      LOW: "하",
-      NONE: "하" // 없음 옵션의 경우 "하" 접두사와 value:0 사용
-    },
-    
-    // 호환성을 위한 부가 정보 - 기존 옵션 구조 유지
-    // 목걸이 옵션
-    necklaceOptions: {
-      special: [
-        // 상 등급 옵션
-        '상:addDamagePer:2.6', // 추가 피해 +2.60%
-        '상:finalDamagePer:1.02', // 적에게 주는 피해 +2.00%
-      ],
-      common: [
-        // 공용 옵션
-        '상:weaponAtkPlus:960', // 무기 공격력 +960
-        '상:atkPlus:390', // 공격력 +390
-      ]
-    },
-    // 귀걸이 옵션
-    earringOptions: {
-      special: [
-        // 상 등급 옵션
-        '상:weaponAtkPer:3', // 무기 공격력 +3.00%
-        '상:atkPer:1.55', // 공격력 +1.55%
-      ],
-      common: [
-        // 공용 옵션
-        '상:weaponAtkPlus:960', // 무기 공격력 +960
-        '상:atkPlus:390', // 공격력 +390
-      ]
-    },
-    // 반지 옵션
-    ringOptions: {
-      special: [
-        // 상 등급 옵션
-        '상:criticalChancePer:1.55', // 치명타 적중률 +1.55%
-        '상:criticalDamagePer:4', // 치명타 피해 +4.00%
-      ],
-      common: [
-        // 공용 옵션
-        '상:weaponAtkPlus:960', // 무기 공격력 +960
-        '상:atkPlus:390', // 공격력 +390
-      ]
-    },
-    
-    // 목걸이 옵션 조합
-    necklaceCombinations: [
-      // 상상 조합
-      {
-        label: '상상-적피추피', 
-        options: ['상:finalDamagePer:1.02', '상:addDamagePer:2.6'] // 적에게 주는 피해 +2.00%, 추가피해 +2.60%
-      },
-      // 상중 조합
-      {
-        label: '상중-적피추피1',
-        options: ['상:finalDamagePer:1.02', '중:addDamagePer:1.6'] // 적에게 주는 피해 +2.00%, 추가피해 +1.60%
-      },
-      {
-        label: '상중-적피추피2',
-        options: ['중:finalDamagePer:1.012', '상:addDamagePer:2.6'] // 적에게 주는 피해 +1.20%, 추가피해 +2.60%
-      },
-      // 상하 조합
-      {
-        label: '상하-적피추피1',
-        options: ['상:finalDamagePer:1.02', '하:addDamagePer:0.7'] // 적에게 주는 피해 +2.00%, 추가피해 +0.70%
-      },
-      {
-        label: '상하-적피추피2',
-        options: ['하:finalDamagePer:1.0055', '상:addDamagePer:2.6'] // 적에게 주는 피해 +0.55%, 추가피해 +2.60%
-      },
-      // 중중 조합
-      {
-        label: '중중-추피적피',
-        options: ['중:addDamagePer:1.6', '중:finalDamagePer:1.012'] // 추가피해 +1.60%, 적에게 주는 피해 +1.20%
-      },
-      // 중하 조합
-      {
-        label: '중하-추피적피1',
-        options: ['하:addDamagePer:0.7', '중:finalDamagePer:1.012'] // 추가피해 +0.70%, 적에게 주는 피해 +1.20%
-      },
-      {
-        label: '중하-추피적피2',
-        options: ['중:addDamagePer:1.6', '하:finalDamagePer:1.0055'] // 추가피해 +1.60%, 적에게 주는 피해 +0.55%
-      },
-      // 하하 조합
-      {
-        label: '하하-추피적피',
-        options: ['하:addDamagePer:0.7', '하:finalDamagePer:1.0055'] // 추가피해 +0.70%, 적에게 주는 피해 +0.55%
-      }
-    ],
-    // 귀걸이 옵션 조합
-    earringCombinations: [
-      // 상상 조합
-      {
-        label: '상상-무공공가',
-        options: ['상:weaponAtkPer:3', '상:atkPer:1.55'] // 무기공격력 +3.00%, 공격력 +1.55%
-      },
-      // 상중 조합
-      {
-        label: '상중-무공공가1',
-        options: ['상:weaponAtkPer:3', '중:atkPer:0.95'] // 무기공격력 +3.00%, 공격력 +0.95%
-      },
-      {
-        label: '상중-무공공가2',
-        options: ['중:weaponAtkPer:1.8', '상:atkPer:1.55'] // 무기공격력 +1.80%, 공격력 +1.55%
-      },
-      // 상하 조합
-      {
-        label: '상하-무공공가1',
-        options: ['상:weaponAtkPer:3', '하:atkPer:0.4'] // 무기공격력 +3.00%, 공격력 +0.40%
-      },
-      {
-        label: '상하-무공공가2',
-        options: ['하:weaponAtkPer:0.8', '상:atkPer:1.55'] // 무기공격력 +0.80%, 공격력 +1.55%
-      },
-      // 중중 조합
-      {
-        label: '중중-공가무공',
-        options: ['중:atkPer:0.95', '중:weaponAtkPer:1.8'] // 공격력 +0.95%, 무기공격력 +1.80%
-      },
-      // 중하 조합
-      {
-        label: '중하-공가무공1',
-        options: ['중:atkPer:0.95', '하:weaponAtkPer:0.8'] // 공격력 +0.95%, 무기공격력 +0.80%
-      },
-      {
-        label: '중하-공가무공2',
-        options: ['하:atkPer:0.4', '중:weaponAtkPer:1.8'] // 공격력 +0.40%, 무기공격력 +1.80%
-      },
-      // 하하 조합
-      {
-        label: '하하-무공공가',
-        options: ['하:weaponAtkPer:0.8', '하:atkPer:0.4'] // 무기공격력 +0.80%, 공격력 +0.40%
-      }
-    ],
-    // 반지 옵션 조합
-    ringCombinations: [
-      // 상상 조합
-      {
-        label: '상상-치피치적',
-        options: ['상:criticalDamagePer:4', '상:criticalChancePer:1.55'] // 치명타 피해 +4.00%, 치명타 적중률 +1.55%
-      },
-      // 상중 조합
-      {
-        label: '상중-치피치적1',
-        options: ['상:criticalDamagePer:4', '중:criticalChancePer:0.95'] // 치명타 피해 +4.00%, 치명타 적중률 +0.95%
-      },
-      {
-        label: '상중-치피치적2',
-        options: ['중:criticalDamagePer:2.4', '상:criticalChancePer:1.55'] // 치명타 피해 +2.40%, 치명타 적중률 +1.55%
-      },
-      // 상하 조합
-      {
-        label: '상하-치피치적1',
-        options: ['상:criticalDamagePer:4', '하:criticalChancePer:0.4'] // 치명타 피해 +4.00%, 치명타 적중률 +0.40%
-      },
-      {
-        label: '상하-치피치적2',
-        options: ['하:criticalDamagePer:1.1', '상:criticalChancePer:1.55'] // 치명타 피해 +1.10%, 치명타 적중률 +1.55%
-      },
-      // 중중 조합
-      {
-        label: '중중-치적치피',
-        options: ['중:criticalChancePer:0.95', '중:criticalDamagePer:2.4'] // 치명타 적중률 +0.95%, 치명타 피해 +2.40%
-      },
-      // 중하 조합
-      {
-        label: '중하-치적치피1',
-        options: ['중:criticalChancePer:0.95', '하:criticalDamagePer:1.1'] // 치명타 적중률 +0.95%, 치명타 피해 +1.10%
-      },
-      {
-        label: '중하-치적치피2',
-        options: ['하:criticalChancePer:0.4', '중:criticalDamagePer:2.4'] // 치명타 적중률 +0.40%, 치명타 피해 +2.40%
-      },
-      // 하하 조합
-      {
-        label: '하하-치피치적',
-        options: ['하:criticalDamagePer:1.1', '하:criticalChancePer:0.4'] // 치명타 피해 +1.10%, 치명타 적중률 +0.40%
-      }
-    ]
+    }
   };
   
   /**
